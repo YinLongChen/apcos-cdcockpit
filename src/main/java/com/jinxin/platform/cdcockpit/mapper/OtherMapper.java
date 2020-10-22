8938cce400e88b0f829abc85561340e9a21128c2
huang lingsong <hlingsong@foxmail.com>
2020-10-22 16:56:57 +0800

    修改

--- src/main/java/com/jinxin/platform/cdcockpit/mapper/OtherMapper.java
+++ src/main/java/com/jinxin/platform/cdcockpit/mapper/OtherMapper.java
@@ -7,6 +7,6 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface OtherMapper {
 
-    @Select("SELECT * FROM( SELECT ENERGY_QUOTA FROM ODS_ENERGY_USETYPE_DATA WHERE BUSI_MONTH = #{time} and ENERGY_TYPE = #{type}) WHERE rownum = 1")
-    Integer selectQuota(@Param("time") String time, @Param("type") String type);
+    @Select("SELECT * FROM( SELECT ENERGY_QUOTA FROM ODS_ENERGY_USETYPE_DATA WHERE ENERGY_AREA_ID = #{ orgId } AND BUSI_MONTH = #{time} and ENERGY_TYPE = #{type}) WHERE rownum = 1")
+    Integer selectQuota(@Param("orgId") String orgId, @Param("time") String time, @Param("type") String type);
 }
--- src/main/java/com/jinxin/platform/cdcockpit/service/impl/OtherServiceImpl.java
+++ src/main/java/com/jinxin/platform/cdcockpit/service/impl/OtherServiceImpl.java
@@ -1,6 +1,7 @@
 package com.jinxin.platform.cdcockpit.service.impl;
 
 import com.jinxin.platform.cdcockpit.mapper.OtherMapper;
+import com.jinxin.platform.cdcockpit.service.CockpitSupportService;
 import com.jinxin.platform.cdcockpit.service.OtherService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,22 +19,25 @@ public class OtherServiceImpl implements OtherService {
     @Autowired
     private OtherMapper otherMapper;
 
+    @Autowired
+    private CockpitSupportService supportService;
+
 
     @Override
     public Integer selectQuota(String type, Integer feild) {
 
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
-
+        String orgId = supportService.getWebCurrUser().getOrgId();
         if (feild.equals(Calendar.MONTH)) {
             LocalDate localDate = LocalDate.now();
             String date = localDate.format(formatter);
-            return otherMapper.selectQuota(date, type);
+            return otherMapper.selectQuota(orgId, date, type);
         } else if (feild.equals(Calendar.YEAR)) {
             LocalDate localDate = LocalDate.now().minusYears(1L);
             Integer sum = 0;
             do {
-                Integer num = otherMapper.selectQuota(localDate.format(formatter), type);
+                Integer num = otherMapper.selectQuota(orgId, localDate.format(formatter), type);
                 sum += num == null ? 0 : num;
                 localDate = localDate.plusMonths(1L);
             } while (localDate.isBefore(LocalDate.now().plusMonths(1L)));
