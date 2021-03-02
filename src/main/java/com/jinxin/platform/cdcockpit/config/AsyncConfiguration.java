c79a92062b9f7315455aea3a345cc1e7cf7d6dba
sunyun <347592389@qq.com>
2021-03-02 14:41:52 +0800

    测试修改bug: Type definition error: [simple type, class java.io.ByteArrayInputStream]

--- src/main/java/com/jinxin/platform/cdcockpit/config/AsyncConfiguration.java
+++ src/main/java/com/jinxin/platform/cdcockpit/config/AsyncConfiguration.java
@@ -1,7 +1,10 @@
 package com.jinxin.platform.cdcockpit.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.concurrent.ArrayBlockingQueue;
@@ -21,4 +24,14 @@ public class AsyncConfiguration {
         return new ThreadPoolExecutor(2, 5, 60L, TimeUnit.SECONDS,
                 new ArrayBlockingQueue<>(5), new ThreadPoolExecutor.AbortPolicy());
     }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(mapper);
+        return converter;
+    }
+
+
 }
